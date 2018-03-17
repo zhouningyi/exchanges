552a32f36998b09568dc2d7f3f0d446b92760e2a
zhouningyi <zny918@126.com>
2018-03-17 23:38:49 +0800

    time

--- utils/request.js
+++ utils/request.js
@@ -2,8 +2,10 @@
 const request = require('request');
 
 function requestPromise (o){
+  const t = new Date();
   return new Promise((resolve, reject) => {
     request(o, (e, res, body) => {
+      console.log(`${new Date() - t}ms...`);
       if (e) return reject(e);
       if (typeof body === 'string') body = JSON.parse(body);
       resolve(body);
