1df4ad19e176fc3cf0ef5ef895ad9e3d624aba06
zhouningyi <zny918@126.com>
2018-04-24 21:13:35 +0800

    参数

--- utils/request.js
+++ utils/request.js
@@ -1,13 +1,18 @@
 
 const request = require('request');
 const Utils = require('./../utils');
+const argv = require('optimist').argv;
+
+const logrest = !!argv.logrest;
 
 function requestPromise(o) {
   const t = new Date();
   return new Promise((resolve, reject) => {
     request(o, (e, res, body) => {
-      const url = `${o.method}: ${(o.uri || o.url).substring(0, 80)}...`;
-      Utils.print(`${new Date() - t}ms...${url}`, 'gray');
+      if (logrest) {
+        const url = `${o.method}: ${(o.uri || o.url).substring(0, 80)}...`;
+        Utils.print(`${new Date() - t}ms...${url}`, 'gray');
+      }
       if (e) return reject(e);
       try {
         if (typeof body === 'string') body = JSON.parse(body);
