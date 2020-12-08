2236c1046b2b6238d9502d7d41231323b9864a73
hz.ren <hz.ren@xufeng-tech.com>
2020-12-09 00:07:12 +0800

    -

--- utils/time_spec.js
+++ utils/time_spec.js
@@ -16,36 +16,34 @@ function main() {
   const nextWeek5 = time.getTimeString(next5, 'day');
   console.log(`今天的下周五是${nextWeek5}`);
   //
-  const settlesQuarter = time.getSettlementTimes(now, 'quarter')
-                  .map(t => time.getTimeString(t, 'day')).join(',');
+  const settlesQuarter = time.getSettlementTimes(now, 'QUARTER').map(t => time.getTimeString(t, 'day')).join(',');
   console.log(`最近的5个季度交割日是${settlesQuarter}`);
-  const thisQSettleDay = time.getFutureSettlementDay(now, 'quarter');
+  const thisQSettleDay = time.getFutureSettlementDay(now, 'QUARTER');
   console.log(`当前的季度交割日是${thisQSettleDay}`);
-  const thisQSettleMoveDay = time.getFutureSettlementMoveDay(now, 'quarter');
+  const thisQSettleMoveDay = time.getFutureSettlementMoveDay(now, 'QUARTER');
   console.log(`当前的季度第一次换仓日是${thisQSettleMoveDay}`);
-  const prevQSettleDay = time.getFutureSettlementDay(now - time.QUARTER, 'quarter');
+  const prevQSettleDay = time.getFutureSettlementDay(now - time.QUARTER, 'QUARTER');
   console.log(`上一个季度交割日是${prevQSettleDay}`);
 
   // 测试一个过去的时间点...
   const testTimeStr = '2018-06-24';
-  const testSettleDay = time.getFutureSettlementDay(testTimeStr, 'quarter');
+  const testSettleDay = time.getFutureSettlementDay(testTimeStr, 'QUARTER');
   console.log(`${testTimeStr}的季度交割日是${testSettleDay}`);
-  const testSettleMoveDay = time.getFutureSettlementMoveDay(testTimeStr, 'quarter');
+  const testSettleMoveDay = time.getFutureSettlementMoveDay(testTimeStr, 'QUARTER');
   console.log(`${testTimeStr}的季度->次周换仓日是${testSettleMoveDay}`);
   //
-  const settlesWeek = time.getSettlementTimes(now, 'this_week')
-  .map(t => time.getTimeString(t, 'day')).join(',');
+  const settlesWeek = time.getSettlementTimes(now, 'THIS_WEEK').map(t => time.getTimeString(t, 'day')).join(',');
   console.log(`最近的5个当周、次周交割日是${settlesWeek}`);
 
-  const thisNsettleDay = time.getFutureSettlementDay(now, 'next_week');
+  const thisNsettleDay = time.getFutureSettlementDay(now, 'NEXT_WEEK');
   console.log(`当前的次周交割日是${thisNsettleDay}`);
-  const thisNsettleDayMove = time.getFutureSettlementMoveDay(now, 'next_week');
+  const thisNsettleDayMove = time.getFutureSettlementMoveDay(now, 'NEXT_WEEK');
   console.log(`当前的次周->当周换仓日是${thisNsettleDayMove}`);
-  const thisTsettleDay = time.getFutureSettlementDay(now, 'this_week');
+  const thisTsettleDay = time.getFutureSettlementDay(now, 'THIS_WEEK');
   console.log(`当前的当周交割日是${thisTsettleDay}`);
 
   //
   base.live();
 }
-
+main()
 module.exports = main;
